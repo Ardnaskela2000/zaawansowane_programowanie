7b575e848ba2e1b7f668e7e6c45ad4a52245cf90
Ardnaskela2000 <Ardnaskela2000@users.noreply.github.com>
2022-12-19 19:42:04 +0100

    ver_4

--- person_detection.py
+++ person_detection.py
@@ -4,21 +4,21 @@ import cv2 as cv
 
 img = cv.imread('C:/Users/ahkha/OneDrive/Obrazy/dev/Photos/ludzie_2.jpg')
 
-cv.imshow('Grupa', img)
+# cv.imshow('Grupa', img)
 
 # Rozpoznanie twarzy z użyciem Haar Cascades
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray Grupa', gray)
+# cv.imshow('Gray Grupa', gray)
 
 haar_cascade = cv.CascadeClassifier('haar_fuulbody.xml')
 
-faces_rect = haar_cascade.detectMultiScale(
-    gray, scaleFactor=1.2, minNeighbors=1)
+body_rect = haar_cascade.detectMultiScale(
+    gray, scaleFactor=1.111, minNeighbors=1)
 
-print(f'Number of faces found = {len(faces_rect)}')
+print(f'Number of faces found = {len(body_rect)}')
 
-for (x, y, w, h) in faces_rect:
+for (x, y, w, h) in body_rect:
     cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 
 cv.imshow('Rozpoznane twarze w Grupa', img)
