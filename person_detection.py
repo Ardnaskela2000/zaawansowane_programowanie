23679ca127c89830f2e97b1e29f97f60300ac2c7
Ardnaskela2000 <Ardnaskela2000@users.noreply.github.com>
2022-12-20 10:50:01 +0100

    ver_7

--- person_detection.py
+++ person_detection.py
@@ -13,8 +13,10 @@ img = imutils.resize(img, width=min(600, img.shape[1]))
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # cv.imshow('Gray Grupa', gray)
 
+#calssifier haarcascade_fullbody.xml
 haar_cascade = cv.CascadeClassifier('haar_fuulbody.xml')
 
+# Wykrycie osoby
 body_rect = haar_cascade.detectMultiScale(
     gray, scaleFactor=1.09, minNeighbors=1)
 
