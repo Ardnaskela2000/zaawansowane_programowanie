5dcf167137a87cddfae8bd3c86cf1432acde7736
Ardnaskela2000 <Ardnaskela2000@users.noreply.github.com>
2022-12-20 11:52:50 +0100

    ver_8

--- person_detection.py
+++ person_detection.py
@@ -3,24 +3,22 @@ import imutils
 
 
 # Obraz
-img = cv.imread('static/ludzie.jpg')
+img = cv.imread('static/ludzie_3.jpg')
 img = imutils.resize(img, width=min(600, img.shape[1]))
 
 # cv.imshow('Grupa', img)
 
-# Rozpoznanie twarzy z użyciem Haar Cascades
-
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # cv.imshow('Gray Grupa', gray)
 
-#calssifier haarcascade_fullbody.xml
+# calssifier haarcascade_fullbody.xml
 haar_cascade = cv.CascadeClassifier('haar_fuulbody.xml')
 
 # Wykrycie osoby
 body_rect = haar_cascade.detectMultiScale(
     gray, scaleFactor=1.09, minNeighbors=1)
 
-print(f'Number of faces found = {len(body_rect)}')
+print(f'Number of people found = {len(body_rect)}')
 
 for (x, y, w, h) in body_rect:
     cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
