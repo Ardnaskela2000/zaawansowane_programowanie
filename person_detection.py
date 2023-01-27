3ad53599ed98d0b078f38e6ab773774135ec7225
Ardnaskela2000 <Ardnaskela2000@users.noreply.github.com>
2023-01-27 18:05:50 +0100

    Update person_detection.py
    ver_10

--- person_detection.py
+++ person_detection.py
@@ -2,27 +2,37 @@ import cv2 as cv
 import imutils
 
 
-# Obraz
-img = cv.imread('static/ludzie.jpg')
-img = imutils.resize(img, width=min(600, img.shape[1]))
+def persondetection(img):
+    # Obraz
+    img = cv.imread(img)
+    img = imutils.resize(img, width=min(600, img.shape[1]))
 
-# cv.imshow('Grupa', img)
+    # cv.imshow('Grupa', img)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-# cv.imshow('Gray Grupa', gray)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # cv.imshow('Gray Grupa', gray)
 
-# calssifier haarcascade_fullbody.xml
-haar_cascade = cv.CascadeClassifier('haar_fuulbody.xml')
+    # calssifier haarcascade_fullbody.xml
+    haar_cascade = cv.CascadeClassifier('haar_fuulbody.xml')
 
-# Wykrycie osoby
-body_rect = haar_cascade.detectMultiScale(
-    gray, scaleFactor=1.09, minNeighbors=1)
+    # Wykrycie osoby
+    body_rect = haar_cascade.detectMultiScale(
+        gray, scaleFactor=1.09, minNeighbors=1)
 
-print(f'Number of people found = {len(body_rect)}')
+    print(f'Number of people found = {len(body_rect)}')
 
-for (x, y, w, h) in body_rect:
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+    for (x, y, w, h) in body_rect:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 
-cv.imshow('Rozpoznane osoby w Grupa', img)
+    cv.imshow('Rozpoznane osoby w Grupa', img)
 
-cv.waitKey(0)
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+
+def showpersondetection():
+    persondetection("static/ludzie.jpg")
+    persondetection("static/ludzie_2.jpg")
+    persondetection("static/ludzie_3.jpg")
+    persondetection("static/ludzie_5.jpg")
